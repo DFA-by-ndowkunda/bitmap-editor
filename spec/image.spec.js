ef2c510c7534c5f424943e5375cb6d3f1250ae2a
ndowkunda <mn057@outlook.com>
2021-03-29 08:27:29 +0100

    add failing test 2

--- spec/image.spec.js
+++ spec/image.spec.js
@@ -1,8 +1,8 @@
 const Image = require('../src/image.js');
 
-describe("Testing commands that are changing image size", function () {
+describe("Change image size", function () {
 
-  fit("can create a new 3 x 3 image with all white pixels", function () {
+  it("can create a new 3 x 3 image with all white pixels", function () {
     let image = new Image(3, 3);
     expect(image.displayImage()).toEqual([
       ['O', 'O', 'O'],
@@ -11,11 +11,19 @@ describe("Testing commands that are changing image size", function () {
     ]);
   });
 
-    fit("can create a new 2 x 2 image with all white pixels", function () {
+    it("can create a new 2 x 2 image with all white pixels", function () {
     let image = new Image(2, 2);
     expect(image.displayImage()).toEqual([
       ['O', 'O'],
       ['O', 'O'],
     ]);
+  });
+});
+
+describe("Change image pixels to colour C", function () {
+
+  it("can create a new 3 x 3 image with all C pixels", function () {
+    let image = new Image(3, 3);
+    expect(image.changeColour(3,3,'C')).toEqual("CCC\nCCC\nCCC");
   });
 });
--- src/image.js
+++ src/image.js
@@ -1,14 +1,22 @@
-const os = require('os');
 
 class Image {
-  constructor(M,N) {
+  constructor(M,N, C='O') {
     this.pixelGrid = Array(M).fill('O').map(pixel => Array(N).fill('O'));
   }
  
   displayImage() {
     return this.pixelGrid
   }
-
+  
+  changeColour(M,N,color) {
+    for (let i = 0; i < M; i++) {
+      this.pixelGrid[i] = color
+      for (let j = 0; j < N; j++) {
+         this.pixelGrid[i][j] = color
+    }
+    }
+    return this.pixelGrid
+  }
  
 }
 module.exports = Image
